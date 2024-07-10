de5f67f815be629b73a5978111ca12e71e8a8231
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2024-07-10 20:09:51 +0530

    useState hook

--- hooks_react/src/App.jsx
+++ hooks_react/src/App.jsx
@@ -1,11 +1,46 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  //useState is a react hook, which creates an "state variable" which helps us to track state in components and updates the user interface when state changes.
+  //syntax use to declare the state
+  //color is the initial value
+  //and setColor is the updated value
+  const [color,setColor] = useState('Violate')
+  
+  const changeColor=()=>{
+    setColor('Red');
+  }
+
+  // const [brand,setBrand] = useState('Ferrari');
+  // const [colour,setColour] = useState('red');
+  // const [year,setYear] = useState('2023');
+  // const [model,setModel] = useState('Rome');
+
+  //instead of using multiple state store it in a key value pair
+  const [car,setCar] = useState({
+    brand:"Ferrari",
+    model:"Roma",
+    year:"2023",
+    colour:"red"
+  })
+
+ const changeYear =()=>{
+    setCar((prev)=>{
+      return {...prev, year:"2024"}
+    })
+  }
   return (
     <>
     <h1>
-      hii
+      My favourite color is {color}!
     </h1>
+    <button onClick={changeColor}>Red</button>
+
+    <h1>My {car.brand}</h1>
+    <h2>It is a {car.colour} {car.brand} from {car.year}</h2>
+    <button onClick={changeYear}>Change year</button>
     </>
   )
 }
