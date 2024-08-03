22929818c86e131f00fcbb6fe7168c51ac2f36a5
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2024-08-03 20:52:07 +0530

    search and get code added

--- hooks_react/src/App.jsx
+++ hooks_react/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
 import { useState, useEffect, useRef } from 'react';
+import data from './TemplateData';
+import './Style.css';
 
 function App() {
 
@@ -7,105 +8,140 @@ function App() {
   //syntax use to declare the state
   //color is the initial value
   //and setColor is the updated value
-  const [color,setColor] = useState('Violate')
+//   const [color,setColor] = useState('Violate')
   
-  const changeColor=()=>{
-    setColor('Red');
-  }
+//   const changeColor=()=>{
+//     setColor('Red');
+//   }
 
-  // const [brand,setBrand] = useState('Ferrari');
-  // const [colour,setColour] = useState('red');
-  // const [year,setYear] = useState('2023');
-  // const [model,setModel] = useState('Rome');
+//   // const [brand,setBrand] = useState('Ferrari');
+//   // const [colour,setColour] = useState('red');
+//   // const [year,setYear] = useState('2023');
+//   // const [model,setModel] = useState('Rome');
 
-  //instead of using multiple state store it in a key value pair
-  const [car,setCar] = useState({
-    brand:"Ferrari",
-    model:"Roma",
-    year:"2023",
-    colour:"red"
-  })
+//   //instead of using multiple state store it in a key value pair
+//   const [car,setCar] = useState({
+//     brand:"Ferrari",
+//     model:"Roma",
+//     year:"2023",
+//     colour:"red"
+//   })
 
- const changeYear =()=>{
-    setCar((prev)=>{
-      return {...prev, year:"2024"}
-    })
-  }
+//  const changeYear =()=>{
+//     setCar((prev)=>{
+//       return {...prev, year:"2024"}
+//     })
+//   }
 
-  const[count, setCount] = useState(0);
+//   const[count, setCount] = useState(0);
 
-  const increseCount=()=>{
-    setCount(count+1)
-  }
+//   const increseCount=()=>{
+//     setCount(count+1)
+//   }
 
-  const decreaseCount=()=>{
-    if(count>0){
-      setCount(count-1)
-    }
-  }
+//   const decreaseCount=()=>{
+//     if(count>0){
+//       setCount(count-1)
+//     }
+//   }
 
-  //useEffect(cb,[])  cb=callback   []=array
-  const[ct,setct]=useState(0);
+//   //useEffect(cb,[])  cb=callback   []=array
+//   const[ct,setct]=useState(0);
  
-  //when we use useEfect hook without any dependency it will execute callback function whenever any state changes
-  //here ct state is changing after every 2 sec
-  useEffect(()=>{
-    setTimeout(()=>{
-      setct(ct=>ct+1);
-    },2000)
-  }, [ct]) //when empty array dependency is added it will execute function only when page is refreshed
-  //when ct is added to the array then it will execute the function when page is loded and when count gets change
+//   //when we use useEfect hook without any dependency it will execute callback function whenever any state changes
+//   //here ct state is changing after every 2 sec
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//       setct(ct=>ct+1);
+//     },2000)
+//   }, [ct]) //when empty array dependency is added it will execute function only when page is refreshed
+//   //when ct is added to the array then it will execute the function when page is loded and when count gets change
 
-  //useRef hook
-  const[value,setValue]=useState(0);
-  const num = useRef(0);
+//   //useRef hook
+//   const[value,setValue]=useState(0);
+//   const num = useRef(0);
 
 
-  //this will run when value changes
-  useEffect(()=>{
-    num.current=num.current+1;
-  },[value])
+//   //this will run when value changes
+//   useEffect(()=>{
+//     num.current=num.current+1;
+//   },[value])
 
-  const inputVal=useRef();
+//   const inputVal=useRef();
 
-  const buttonclicked=()=>{
-    console.log(inputVal.current)
-    inputVal.current.style.background='blue';
-  }
+//   const buttonclicked=()=>{
+//     console.log(inputVal.current)
+//     inputVal.current.style.background='blue';
+//   }
   
-  return (
-    <>
-    <h1><i>USESTATE HOOK</i></h1>
-    <h1>
-      My favourite color is {color}!
-    </h1>
-    <button onClick={changeColor}>Red</button>
+//   return (
+ //    <>
+//     //<h1><i>USESTATE HOOK</i></h1>
+//     <h1>
+//       My favourite color is {color}!
+//     </h1>
+//     <button onClick={changeColor}>Red</button>
 
-    <h1>My {car.brand}</h1>
-    <h2>It is a {car.colour} {car.brand} from {car.year}</h2>
-    <button onClick={changeYear}>Change year</button>
+//     <h1>My {car.brand}</h1>
+//     <h2>It is a {car.colour} {car.brand} from {car.year}</h2>
+//     <button onClick={changeYear}>Change year</button>
 
-    <h3>Count: {count}</h3>
-    <button onClick={increseCount}>Increase count</button>
-    <button onClick={decreaseCount}>Decrease count</button>
+//     <h3>Count: {count}</h3>
+//     <button onClick={increseCount}>Increase count</button>
+//     <button onClick={decreaseCount}>Decrease count</button>
 
-    <h1><i>USEEFFECT HOOK</i></h1>
-    <h3>I have rendered {ct} times</h3>
+//     <h1><i>USEEFFECT HOOK</i></h1>
+//     <h3>I have rendered {ct} times</h3>
 
-    <h1>USEREF HOOK</h1>
-    <button onClick={()=>{setValue(prev=>prev-1)}}>-1</button>
-    <h1>{value}</h1>
-    <button onClick={()=>{setValue(prev=>prev+1)}}>+1</button>
-    <h3>Render count: {num.current}</h3>
+//     <h1>USEREF HOOK</h1>
+//     <button onClick={()=>{setValue(prev=>prev-1)}}>-1</button>
+//     <h1>{value}</h1>
+//     <button onClick={()=>{setValue(prev=>prev+1)}}>+1</button>
+//     <h3>Render count: {num.current}</h3>
 
-    <input type='text' ref={inputVal}/>
-    <br/><br/>
-    <button onClick={buttonclicked}>Click here</button>
-    </>
+//     <input type='text' ref={inputVal}/>
+//     <br/><br/>
+//     <button onClick={buttonclicked}>Click here</button>
+ //   </>
 
     
     
-  )
+ // )
+
+ const[searchItem,setSearchItem]=useState("");
+ return(
+  <>
+   <div className='templateContainer'>
+    <div className='searchInput_container'>
+      <input id="searchinput" type='text' placeholder='search here...' onChange={(event)=>{
+        setSearchItem(event.target.value);
+      }} />
+    </div>
+
+    <div className='template_container'>
+      {
+        data.filter((val) => {
+          if(searchItem===""){
+            return val;
+          } else if(val.title.toLowerCase().includes(searchItem.toLowerCase())){
+            return val;
+          }
+          return null;
+        })
+        .map((val) => {
+          return(
+            <div className='template' key={val.id}>
+              <img src={val.image} alt={val.title} />
+              <h3>{val.title}</h3>
+              <p className='price'>${val.price}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+   </div>
+  </>
+ )
 }
 
 export default App
---
+++ hooks_react/src/TemplateData.js
@@ -0,0 +1,26 @@
+import iclever from './assets/images/iclever kids headphones.jpg';
+import noise from './assets/images/noise headphones.jpg';
+import sony from './assets/images/sony headphones.jpg';
+const data=[
+    {
+        "id":1,
+        "image":sony,
+        "title":"Sony Headphones",
+        "price":1800
+    },
+     
+    {
+        "id":1,
+        "image":noise,
+        "title":"Noise Headphones",
+        "price":2000
+    },
+
+    {
+        "id":1,
+        "image":iclever,
+        "title":"Iclever Kids Headphones",
+        "price":1500
+    }
+]
+export default data;
